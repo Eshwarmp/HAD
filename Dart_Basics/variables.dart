54485904081cda2be7a5e9d7f3d55f8a87aeca2b
Eshwar <eshwarmp02@gmail.com>
2026-03-08 17:02:26 +0530

    Introduction Variables and their declaration

---
+++ Dart_Basics/variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  int age = 32;
+  var age2 = 22;
+  print(age);
+  double exp = 2.3e2;
+  int hex = 0xEAC;
+  bool isAlive = true;
+  print(age);
+  print(age2);
+  print(exp);
+  print(hex);
+  print(isAlive);
+}
